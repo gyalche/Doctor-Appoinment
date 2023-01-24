8cf4c83222d73946c71162acb42b8b4b9b6c4ae0
Dawa <sherpadawa01@outlook.com>
2023-01-24 21:34:18 +0545

    work started for doctor appointment web application

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
+import colors from 'colors';
+
+const app = express();
+dotenv.config();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(morgan('dev'));
+app.use(helmet());
+
+const PORT = process.env.PORT || 500;
+app.listen(PORT, () => {
+  console.log(`server is running on localhost://${PORT}`.bgCyan.white);
+});
